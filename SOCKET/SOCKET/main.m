541a7c692932a04695f34cc3d1fcd8665fe33539
suxinde2009 <suxinde2009@126.com>
2017-08-11 00:19:27 +0800

    Init

---
+++ SOCKET/SOCKET/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SOCKET
+//
+//  Created by suxinde on 2017/8/11.
+//  Copyright © 2017年 com.su. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
